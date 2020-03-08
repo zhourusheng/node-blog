54e692b26801d39e19bbda3d7592dce8fce25bea
zhourusheng <nfuzrs@126.com>
2020-03-08 10:48:36 +0800

    处理postData

--- app.js
+++ app.js
@@ -13,18 +13,51 @@ const serverHandle = (req, res) => {
   // 解析 query
   req.query = querystring.parse(url.split('?')[1])
 
-  // 处理路由
-  const blogData = handleBlogRouter(req, res)
-  if (blogData) {
-    res.end(JSON.stringify(blogData))
-    return // 注意 return
+  // 处理 post data
+  const getPostData = req => {
+    const promise = new Promise((resolve, reject) => {
+      // 判断 method
+      if (req.method !== 'POST') {
+        resolve({})
+        return
+      }
+      // 判断 content-type
+      if (req.headers['content-type'] !== 'application/json') {
+        resolve({})
+        return
+      }
+      // 处理数据
+      let postData = ''
+      req.on('data', chunk => {
+        postData += chunk
+      })
+      req.on('end', () => {
+        if (!postData) {
+          resolve({})
+          return
+        }
+        return reject(JSON.parse(postData))
+      })
+    })
+    return promise
   }
 
-  const userData = handleUserRouter(req, res)
-  if (userData) {
-    res.end(JSON.stringify(userData))
-    return // 注意 return
-  }
+  getPostData(req).then(postData => {
+    req.body = postData
+
+    // 处理路由
+    const blogData = handleBlogRouter(req, res)
+    if (blogData) {
+      res.end(JSON.stringify(blogData))
+      return // 注意 return
+    }
+
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+      res.end(JSON.stringify(userData))
+      return // 注意 return
+    }
+  })
 
   // 处理 404
   res.writeHead(404, { 'Content-type': 'text/plain' })
